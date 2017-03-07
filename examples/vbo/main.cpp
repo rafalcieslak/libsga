293123e59b6f7c2e738fe8559ae33f3cd1735d81
Rafał Cieślak <rafalcieslak256@gmail.com>
2017-03-07 22:01:43 +0100

    Made the VBO demo a bit more interesting

--- examples/vbo/main.cpp
+++ examples/vbo/main.cpp
@@ -15,24 +15,25 @@ int main(){
   auto vbo = sga::VBO<sga::DataType::Float2,
                       sga::DataType::UByte4>::create(3);
   
-  std::vector<CustomData> f = {
-    { {  0.0f, -0.5f },{ 0xFF, 0x00, 0x00, 0xFF }, },
-    { {  0.5f,  0.5f },{ 0x00, 0x00, 0xFF, 0xFF }, },
-    { { -0.5f,  0.5f },{ 0x00, 0xFF, 0x00, 0xFF }, },
-  };
-  vbo->write(f);
-  
   pipeline->setTarget(window);
   
-  window->setFPSLimit(30);
+  window->setFPSLimit(60);
   
   while(window->isOpen()){
-    
-    double q = sga::getTime() * 2.5;
+    double q = sga::getTime() * 3.0;
     double r = std::sin(q + 0.00 * M_PI * 2);
     double g = std::sin(q + 0.33 * M_PI * 2);
     double b = std::sin(q + 0.66 * M_PI * 2);
-    pipeline->setClearColor(r, g, b);
+    uint8_t R = std::max(0.0, r*255);
+    uint8_t G = std::max(0.0, g*255);
+    uint8_t B = std::max(0.0, b*255);
+    
+    std::vector<CustomData> f = {
+      { {  0.0f, -0.5f },{ R, G, B, 0xFF }, },
+      { {  0.5f,  0.5f },{ G, B, R, 0xFF }, },
+      { { -0.5f,  0.5f },{ B, R, G, 0xFF }, },
+    };
+    vbo->write(f);
   
     pipeline->drawVBO(vbo);
     window->nextFrame();

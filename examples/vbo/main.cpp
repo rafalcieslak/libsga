92747ebd3b563b92763de23c90f2353f42bcc46e
Rafał Cieślak <rafalcieslak256@gmail.com>
2017-03-09 19:16:42 +0100

    Cleaned up VBO example

--- examples/vbo/main.cpp
+++ examples/vbo/main.cpp
@@ -2,9 +2,6 @@
 
 #include <sga.hpp>
 
-extern char const *testVertShaderText;
-extern char const *testFragShaderText;
-
 struct __attribute__((packed)) CustomData{
   float position[2];
   float color[4];
@@ -12,14 +9,25 @@ struct __attribute__((packed)) CustomData{
 
 int main(){
   sga::init();
-  auto window = sga::Window::create(480, 360, "Example window");
+  auto window = sga::Window::create(500, 500, "Example window");
   auto pipeline = sga::Pipeline::create();
 
   auto vbo = sga::VBO<sga::DataType::Float2,
                       sga::DataType::Float4>::create(3);
 
-  auto vertShader = sga::VertexShader::createFromSource(testVertShaderText);
-  auto fragShader = sga::FragmentShader::createFromSource(testFragShaderText);
+  auto vertShader = sga::VertexShader::createFromSource(R"(
+    void main()
+    {
+      outColor = inColor;
+      gl_Position = vec4(inVertex, 0, 1);
+    }
+  )");
+  auto fragShader = sga::FragmentShader::createFromSource(R"(
+    void main()
+    {
+      outColor = inColor;
+    }
+  )");
 
   vertShader->addInput(sga::DataType::Float2, "inVertex");
   vertShader->addInput(sga::DataType::Float4, "inColor");
@@ -39,17 +47,17 @@ int main(){
 
   while(window->isOpen()){
     double q = sga::getTime() * 3.0;
-    float r = std::sin(q + 0.00 * M_PI * 2);
-    float g = std::sin(q + 0.33 * M_PI * 2);
-    float b = std::sin(q + 0.66 * M_PI * 2);
-    float R = std::max(0.0, std::pow(r, 0.8));
-    float G = std::max(0.0, std::pow(g, 0.8));
-    float B = std::max(0.0, std::pow(b, 0.8));
+    float x1 =  std::sin(q + 0.00 * M_PI * 2);
+    float x2 =  std::sin(q + 0.33 * M_PI * 2);
+    float x3 =  std::sin(q + 0.66 * M_PI * 2);
+    float y1 = -std::cos(q + 0.00 * M_PI * 2);
+    float y2 = -std::cos(q + 0.33 * M_PI * 2);
+    float y3 = -std::cos(q + 0.66 * M_PI * 2);
     
     std::vector<CustomData> f = {
-      { {  0.0f, -0.5f },{ R, G, B, 1.0 }, },
-      { {  0.5f,  0.5f },{ G, B, R, 1.0 }, },
-      { { -0.5f,  0.5f },{ B, R, G, 1.0 }, },
+      { { x1, y1 },{ 1, 0, 0, 1 }, },
+      { { x2, y2 },{ 0, 1, 0, 1 }, },
+      { { x3, y3 },{ 0, 0, 1, 1 }, },
     };
     vbo->write(f);
   
@@ -58,19 +66,3 @@ int main(){
   }
   sga::terminate();
 }
-
-
-char const *testVertShaderText = R"(
-void main()
-{
-  outColor = inColor;
-  gl_Position = vec4(inVertex, 0, 1);
-}
-)";
-
-char const *testFragShaderText = R"(
-void main()
-{
-  outColor = inColor;
-}
-)";
